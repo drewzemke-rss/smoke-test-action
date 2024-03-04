7f46fccf560bfa2e6d8db85bfc447d2feed7e855
Drew Zemke <dnzemke@ucdavis.edu>
2024-03-04 09:07:58 -0800

    feat(smoke): first pass at an actual smoke test action

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
 import * as core from '@actions/core';
+import retry from 'async-retry';
 
-core.setOutput('message', 'Ohhh hello!! But from typescript this time 😮');
-console.log('all done!');
+import axios from 'axios';
+
+const healthUrl = core.getInput('url');
+
+const checkHealth = async (bail: any) => {
+  console.log('Making a call to server with url', healthUrl);
+  const res = await axios.get(healthUrl);
+  console.log('res', res.data);
+
+  if (res.status === 200) {
+    return res.data;
+  }
+
+  if (res.status === 404) {
+    bail(new Error('Server not up'));
+  }
+
+  return bail(new Error(`unknown error: ${res.status}`));
+};
+
+const retries = Number(core.getInput('retries'));
+const factor = Number(core.getInput('factor'));
+const maxTimeout = Number(core.getInput('wait_seconds')) * 60 * 1000;
+
+retry(checkHealth, { retries, factor, maxTimeout })
+  .then((value) => {
+    console.log(value);
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
